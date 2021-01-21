b22095af42738ac5291384a04cff8032e82a7275
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-20 22:31:01 -0600

    js updated

---
+++ script.js
@@ -0,0 +1,5 @@
+var currentDate = luxon.DateTime.local();
+var topDate = $("#currentDay");
+var printDate = currentDate.toLocaleString(luxon.DateTime.DATE_MED_WITH_WEEKDAY)
+topDate.text(printDate);
+console.log(printDate);
