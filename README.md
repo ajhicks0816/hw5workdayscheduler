59f5a5bcbb1a61bb46a2f97c7413cb6da1a92a8a
Alaina Hicks <ajhicks0816@gmail.com>
2021-01-22 00:11:01 -0600

    update to readme, adding code to html and js files

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # hw5workdayscheduler
 
 ## Purpose/Description
+The purpose of this homework assignment is to create a a basic calendar application that allows a user to save events for each hour of the workday. 2
 
 ## Usage
 
@@ -12,4 +13,4 @@
 
 ---
 Git Hub [https://github.com/ajhicks0816/hw5workdayscheduler]  
-Work Day Scheduler [https://ajhicks0816.github.io/hw5workdayscheduler/]
+Work Day Scheduler [https://ajhicks0816.github.io/hw5workdayscheduler/]41459999
--- script.js
+++ script.js
@@ -3,3 +3,24 @@ var topDate = $("#currentDay");
 var printDate = currentDate.toLocaleString(luxon.DateTime.DATE_MED_WITH_WEEKDAY)
 topDate.text(printDate);
 console.log(printDate);
+
+// var todo;
+// function storetodo() {
+//     todo = $(this)
+// var todoId = todo.attr("id")
+// var userTodo =$("textarea#" + todoId).val()
+// console.log("id:" + todoId  + " , task: " + userTodo)
+// localStorage.setItem(todoId, userTodo);
+// //  console.log($(this))
+// //  console.log(todo.data("first"))
+// }
+//  function loadtodos() {
+//      for (let i = 0; i < 24; i++) {
+//          console.log("loading data for " + i) 
+//         $("textarea#" + i).text(localStorage.getItem(i))
+//      }
+//  }
+//  $(document).ready(function(){
+//      $(".saveBtn").on("click", storetodo);
+//      loadtodos()
+//  })
